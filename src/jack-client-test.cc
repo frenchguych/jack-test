a9afe3266e5728b7b75d2a1f924f9d3add7a9c06
Yannick Mauray <yannick.mauray@gmail.com>
2019-04-06 02:00:15 +0200

    Added verbosity

--- src/jack-client-test.cc
+++ src/jack-client-test.cc
@@ -18,20 +18,31 @@ int main()
 		}
 		exit(1);
 	}
+
+	printf("Jack client successfully connected !\n");
+
 	if (status & JackServerStarted) {
 		fprintf (stderr, "JACK server started\n");
 	}
+
 	if (status & JackNameNotUnique) {
 		client_name = jack_get_client_name(client);
 		fprintf (stderr, "unique name `%s' assigned\n", client_name);
 	}
 
 	if (jack_activate (client)) {
-	fprintf (stderr, "cannot activate client");
+		fprintf (stderr, "cannot activate client");
 		exit (1);
 	}
 
+	jack_nframes_t sample_rate = jack_get_sample_rate(client);
+	printf("Server's sample rate is %d\n", sample_rate);
+
+	float cpu_load = jack_cpu_load(client);
+	printf("Server's CPU load is %f\n", cpu_load);
+
 	jack_client_close (client);
+	printf("Disconnected from server\n");
 
 	return 0;
 }
